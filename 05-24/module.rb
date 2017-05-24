53a79d6da7d0e3e5eab597327d9a77cb38fe92dd
Karl Stolley <karl.stolley@gmail.com>
2017-05-24 11:23:28 -0500

    Added Vehicle module and Car class from yesterday

---
+++ 05-24/module.rb
@@ -0,0 +1,32 @@
+module Vehicle
+  def accelerate
+    @speed += 1
+    how_fast
+  end
+  def brake
+    if @speed > 0
+      @speed -= 1
+    end
+    how_fast
+  end
+  def how_fast
+    if @speed == 0
+      puts "You are stopped. From a module."
+    else
+      puts "You are now going #{@speed} miles per hour. From a module."
+    end
+  end
+end
+
+class Car
+  include Vehicle
+  attr_accessor :speed, :make
+  def initialize(make)
+    @speed = 0
+    @make = make
+    puts "You have a car! It's a #{@make}!"
+  end
+  def how_fast
+    puts "This car is broken. It can't move."
+  end
+end
