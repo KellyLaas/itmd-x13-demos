4a8bdfb8371d53e732dd3ac02868dd1e2d72178f
Karl Stolley <karl.stolley@gmail.com>
2017-06-06 12:47:41 -0500

    Run migration for Course model

--- scheduler/db/schema.rb
+++ scheduler/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606172703) do
+ActiveRecord::Schema.define(version: 20170606174539) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "subject"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "schedules", force: :cascade do |t|
     t.string "term"
