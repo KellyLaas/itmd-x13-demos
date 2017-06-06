882efeb82cf22cf65497b6a558eeab64714f45a8
Karl Stolley <karl.stolley@gmail.com>
2017-06-06 12:33:04 -0500

    Run migration to add foreign key to Schedule

--- scheduler/db/schema.rb
+++ scheduler/db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606171606) do
+ActiveRecord::Schema.define(version: 20170606172703) do
 
   create_table "schedules", force: :cascade do |t|
     t.string "term"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "student_id"
+    t.index ["student_id"], name: "index_schedules_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|
