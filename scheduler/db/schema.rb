9082331b1e975843b0f7f4c67b6fd64108779ceb
Karl Stolley <karl.stolley@gmail.com>
2017-06-06 12:54:23 -0500

    Run migration for join table

--- scheduler/db/schema.rb
+++ scheduler/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606174539) do
+ActiveRecord::Schema.define(version: 20170606174948) do
 
   create_table "courses", force: :cascade do |t|
     t.string "subject"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "courses_schedules", id: false, force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "schedule_id"
+    t.index ["course_id"], name: "index_courses_schedules_on_course_id"
+    t.index ["schedule_id"], name: "index_courses_schedules_on_schedule_id"
   end
 
   create_table "schedules", force: :cascade do |t|
