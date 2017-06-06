f48f346a617770cf2abd96cc9f4938e8c8f5914b
Karl Stolley <karl.stolley@gmail.com>
2017-06-06 12:53:11 -0500

    Write migration for course/schedule join

--- scheduler/db/migrate/20170606174948_create_courses_schedules_join_table.rb
+++ scheduler/db/migrate/20170606174948_create_courses_schedules_join_table.rb
@@ -1,4 +1,8 @@
 class CreateCoursesSchedulesJoinTable < ActiveRecord::Migration[5.1]
   def change
+    create_table :courses_schedules, id: false do |t|
+      t.belongs_to :course, index: true
+      t.belongs_to :schedule, index: true
+    end
   end
 end
