bf155fdaa7ccec39e0bd3f7c7a75ec5c85b76b7b
Karl Stolley <karl.stolley@gmail.com>
2017-06-07 11:42:04 -0500

    Test for subject and location on Course

--- scheduler/test/models/course_test.rb
+++ scheduler/test/models/course_test.rb
@@ -4,4 +4,10 @@ class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "must have a subject and a location" do
+    @course = Course.new
+    assert @course.invalid?
+    assert @course.errors[:subject].any?
+    assert @course.errors[:location].any?
+  end
 end
