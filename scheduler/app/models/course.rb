849c95e3bf11d64933c29acd3b16852e611ca135
Karl Stolley <karl.stolley@gmail.com>
2017-06-07 11:59:26 -0500

    Validate Course subject format
    Also includes good and bad test cases for the regexp.

--- scheduler/app/models/course.rb
+++ scheduler/app/models/course.rb
@@ -1,4 +1,8 @@
 class Course < ApplicationRecord
   has_and_belongs_to_many :schedules
   validates :subject, :location, presence: true
+  validates :subject, format: {
+    with: /\A[A-Z]{2,4} \d{3}\z/,
+    message: "subject must be XXXX 123"
+  }
 end
--- scheduler/test/models/course_test.rb
+++ scheduler/test/models/course_test.rb
@@ -10,4 +10,18 @@ class CourseTest < ActiveSupport::TestCase
     assert @course.errors[:subject].any?
     assert @course.errors[:location].any?
   end
+
+  test "subject must be 2-4 letters, 3 numbers" do
+    good = ['CS 104', 'COM 001', 'ITMD 413']
+    bad = ['A 111', 'Humanities 151', 'com 222', 'ITMD 22', 'ITMD 2222', 'CS', '251', 'ITMD 513 SOMETHING', "ITMD 513\n\npwned"]
+    def new_subject(subject)
+      course = Course.new(location: "SH 218", subject: subject)
+    end
+    good.each do |subject|
+      assert new_subject(subject).valid?, "#{subject} should be valid"
+    end
+    bad.each do |subject|
+      assert new_subject(subject).invalid?, "#{subject} should not be valid"
+    end
+  end
 end
