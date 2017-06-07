2d618dcdd8f71a966d46d72cc7734d9e759a26e8
Karl Stolley <karl.stolley@gmail.com>
2017-06-07 10:56:34 -0500

    Add name-handling methods to Student

--- scheduler/app/models/student.rb
+++ scheduler/app/models/student.rb
@@ -1,4 +1,14 @@
 class Student < ApplicationRecord
   has_one :schedule
   accepts_nested_attributes_for :schedule
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def name_from_parts(name)
+    name_parts = name.split(" ")
+    self.last_name = name_parts.pop
+    self.first_name = name_parts.join(" ")
+  end
 end
