35652c328f3befb31d7a0f3e286e0022a29c2a06
Karl Stolley <karl.stolley@gmail.com>
2017-06-13 18:13:59 -0500

    Point to students from courses

--- scheduler/app/models/course.rb
+++ scheduler/app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   has_and_belongs_to_many :schedules
+  has_many :students, through: :schedules
   validates :subject, :location, presence: true
   validates :subject, format: {
     with: /\A[A-Z]{2,4} \d{3}\z/,
