4e697dd55968df458e59d15c384948e1121026e2
Karl Stolley <karl.stolley@gmail.com>
2017-06-20 22:27:59 -0500

    Wire up remaining session actions and login view

--- userland/app/controllers/sessions_controller.rb
+++ userland/app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token
+
+  def login
+    if current_user
+      redirect_to posts_url
+    end
+  end
+
   def create
     auth_hash = request.env['omniauth.auth']
     @user = User.find_or_create_by(uid: auth_hash['uid']) do |user|
@@ -7,5 +14,11 @@ class SessionsController < ApplicationController
       user.nickname = auth_hash['info']['nickname']
     end
     self.current_user = @user
+    redirect_to posts_url
+  end
+
+  def logout
+    reset_session
+    redirect_to login_url, notice: "You have been successfully logged out."
   end
 end
