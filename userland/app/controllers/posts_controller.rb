11ade50f068a869b3c0492bc43d468a06f3d5b9c
Karl Stolley <karl.stolley@gmail.com>
2017-06-21 13:00:07 -0500

    Use controller to prevent deleting others' work

--- userland/app/controllers/posts_controller.rb
+++ userland/app/controllers/posts_controller.rb
@@ -57,10 +57,14 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
+    if @post.user.uid == current_user.uid
+      @post.destroy
+      respond_to do |format|
+        format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to posts_url, notice: "You can only delete your own stuff"
     end
   end
 
