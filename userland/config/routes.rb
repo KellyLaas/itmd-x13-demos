1b2ba90b7d1b5db0dc242f097b25e0234310061b
Karl Stolley <karl.stolley@gmail.com>
2017-06-20 22:26:02 -0500

    Set up named routes for login and logout

--- userland/config/routes.rb
+++ userland/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
   match '/auth/:provider/callback', to: 'sessions#create', via: [:post,:get]
+  get '/auth/github', as: 'github_login'
+  get '/login', to: 'sessions#login', as: 'login'
+  get '/logout', to: 'sessions#logout', as: 'logout'
 end
