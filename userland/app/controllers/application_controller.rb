7fd054fa2ffaeab29114384fb25515428d47ae27
Karl Stolley <karl.stolley@gmail.com>
2017-06-20 21:57:41 -0500

    Add app controller methods for current user

--- userland/app/controllers/application_controller.rb
+++ userland/app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by_uid(session[:user_uid])
+  end
+
+  helper_method :current_user
+
+  def current_user=(user)
+    @current_user = user
+    session[:user_uid] = @current_user.uid
+    session[:user_name] = @current_user.name
+  end
+
 end
