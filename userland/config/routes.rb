d6a930b9b73ca3fcf9db400b812a01d57b4ceb34
Karl Stolley <karl.stolley@gmail.com>
2017-06-20 21:18:56 -0500

    Add match on GET for third-party providers

--- userland/config/routes.rb
+++ userland/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:post]
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:post,:get]
 end
