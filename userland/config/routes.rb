d19c1e35aa7195cd64d61ed26d2f995db353e589
Karl Stolley <karl.stolley@gmail.com>
2017-06-21 10:54:46 -0500

    Improve callback route for post and get

--- userland/config/routes.rb
+++ userland/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:post,:get]
 end
