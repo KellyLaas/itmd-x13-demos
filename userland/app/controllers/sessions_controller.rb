70160b5bf6c5c2b6d595716df8c682213e604e69
Karl Stolley <karl.stolley@gmail.com>
2017-06-28 14:57:18 -0500

    Reset session upon login

--- userland/app/controllers/sessions_controller.rb
+++ userland/app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    reset_session
     auth_hash = request.env['omniauth.auth']
     @user = User.find_or_create_by(uid: auth_hash['uid']) do |user|
       user.name = auth_hash['info']['name']
